382f49e410646c800bb426c88acbd924351b70f0
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2026-06-28 22:33:10 +0200

    Add missing PHPStan packages and fix code style on scaffold

--- src/bundle/IbexaDoctrineMigrationsBundle.php
+++ src/bundle/IbexaDoctrineMigrationsBundle.php
@@ -10,6 +10,4 @@ namespace Ibexa\Bundle\DoctrineMigrations;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-final class IbexaDoctrineMigrationsBundle extends AbstractBundle
-{
-}
+final class IbexaDoctrineMigrationsBundle extends AbstractBundle {}
