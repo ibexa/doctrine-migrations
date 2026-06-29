2678eb50b78e6cb6f4b0f055d4a42d74fd8ba0ec
Paweł Niedzielski <pawel.niedzielski@ibexa.co>
2026-06-29 02:51:57 +0200

    Fix PHPStan array type annotation and CS types_spaces violation

--- tests/lib/Migration/ServiceMigrationsRepositoryTest.php
+++ tests/lib/Migration/ServiceMigrationsRepositoryTest.php
@@ -269,7 +269,7 @@ final class ServiceMigrationsRepositoryTest extends TestCase
             static fn (string $id): bool => $hasMap[$id] ?? false,
         );
         $container->method('get')->willReturnCallback(
-            static fn (string $id): AbstractMigration|null => $getMap[$id] ?? null,
+            static fn (string $id): AbstractMigration | null => $getMap[$id] ?? null,
         );
         $container->method('getProvidedServices')->willReturn($providedServices);
 
